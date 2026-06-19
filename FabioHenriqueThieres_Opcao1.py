c9b9f341bf6f98caec8ada118846a5e568470bb1
Fabio Thieres <fabio.santana@aluno.fmpsc.edu.br>
2026-06-19 19:59:53 -0300

    Add files via upload

--- FabioHenriqueThieres_Opcao1.py
+++ FabioHenriqueThieres_Opcao1.py
@@ -29,7 +29,6 @@ while True:
     if op == 1:
         numero = 0
         for lanche in cardapio:
-            preco = cardapio[lanche]
             print(f'{numero}. {lanche}: {cardapio[lanche]}')
             numero += 1
     elif op == 2:
@@ -38,51 +37,26 @@ while True:
         preco_do_lanche = cardapio[lanche_escolhido]
         pedidos.append(lanche_escolhido)
         comanda += preco_do_lanche
-        print(f'pedidos: {pedidos}')
-        print(f'comanda: {comanda}')
     elif op == 3:
+        if len(pedidos) == 0:
+            print('O carrinho está vazio! Não há nada à ser removido.')
+        else:
+            for i in range(len(pedidos)):
+                print(f'{i}. {pedidos[i]}')
+            rem = int(input('Deseja remover qual item? '))
+            lanche_removido = pedidos.pop(rem)
+            comanda -= cardapio[lanche_removido]
+    elif op == 4:
+        print('Pedidos: ')
         for i in range(len(pedidos)):
-            print(i+pedidos[i])
-        rem = int(input('Deseja remover qual item? '))
-        
+            print(pedidos[i])
+    elif op == 5:
+        print(f'Valor total do pedido: {comanda}')
+    elif op == 6:
+        print('Até mais! ')
+        break
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# cardapio = [
-#     'x-bacon: R$ 25.00',
-#     'x-burger: R$ 20.00',
-#     'x-egg: R$ 22.00',
-#     'refrigerante lata: R$ 7.00',
-#     'água s/ gás: R$5.00'
-# ]
-
-# pedido = []
-
-# while True:
-#     if op == '1':
-#         for i, item in enumerate(cardapio):
-#             print(f'\n{i}.{item}')
-        
-#         des = int(input('\nO que deseja pra hoje? (0, 1, 2...) '))
-        
-#         if des == 0:
-#             pedido.append(cardapio[0])
-#             print(pedido)
-#     elif op == '2':
-#         print('Perfeito, o que gostaria?')
-
-#     elif op == '7':
-#         print('Operação cancelada. Obrigado por visitar nossa lanchonete!')
-#         break
+print('Desenvolvido por: Fabio Thieres e Duda')
+print('Github: fabiothieres')
+print('LinkedIn: Fabio Thieres')
